b90d4d98720814170f227a82b3bc6c685fdc4201
JinJieBeWater <cjj20030813@qq.com>
2025-03-24 02:30:31 +0800

    ✨ feat(md-editor): 添加页面更新乐观更新
    - 添加页面更新失败时的回滚逻辑
    - 使用 toast 显示友好的错误提示
    - 优化缓存数据管理，防止数据不一致

--- src/app/dashboard/page/_components/md-editor.tsx
+++ src/app/dashboard/page/_components/md-editor.tsx
@@ -5,6 +5,7 @@ import { SettingsProvider } from "@/components/editor/settings";
 import { api } from "@/trpc/react";
 import { type Page } from "@/types/page";
 import { type Value } from "@udecode/plate";
+import { toast } from "sonner";
 import { useDebounceCallback } from "usehooks-ts";
 
 interface MdEditorProps {
@@ -15,10 +16,22 @@ export function MdEditor({ page }: MdEditorProps) {
   const utils = api.useUtils();
 
   const updatePage = api.page.update.useMutation({
-    onSuccess: async () => {
-      await utils.page.get.invalidate({
+    onMutate: () => {
+      // 从 queryCache 中获取数据
+      const prevData = utils.page.get.getData({
         id: page.id,
       });
+      return { prevData };
+    },
+    onError(_err, _newPage, ctx) {
+      // 当修改失败后，使用来自 onMutate 中的值
+      utils.page.get.setData(
+        {
+          id: page.id,
+        },
+        ctx?.prevData,
+      );
+      toast.error("please try again later or check your network");
     },
   });
 
