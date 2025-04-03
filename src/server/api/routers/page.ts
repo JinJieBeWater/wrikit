dbf1cbd9993b4fc5a4da45b1a62952701aeac46b
JinJieBeWater <cjj20030813@qq.com>
2025-04-03 17:03:02 +0800

    ✨ feat(page): 修改create逻辑 适配闭包表
    - 【页面创建】修改页面创建逻辑，适配闭包表结构
    - 【事务处理】使用事务保证数据一致性
    - 【ID生成】如果input中没有id，生成一个随机id
    - 【页面路径】插入自引用路径，并处理父页面路径
    - 【父页面处理】如果存在父页面，添加所有父页面到当前页面的路径

--- src/server/api/routers/page.ts
+++ src/server/api/routers/page.ts
@@ -1,5 +1,5 @@
 import { createTRPCRouter, protectedProcedure } from "@/server/api/trpc";
-import { pagesPinned, pages } from "@/server/db/schema";
+import { pagesPinned, pages, pagesPath } from "@/server/db/schema";
 import { PageTypeArray } from "@/types/page";
 import { and, count, eq, inArray, like } from "drizzle-orm";
 import { z } from "zod";
@@ -170,14 +170,60 @@ export const pageRouter = createTRPCRouter({
     .meta({ description: "创建新页面" })
     .input(createPageZod)
     .mutation(async ({ ctx, input }) => {
-      const [insertedPage] = await ctx.db
-        .insert(pages)
-        .values({
-          ...input,
-          createdById: ctx.session.user.id,
-        })
-        .returning();
-      return insertedPage;
+      await ctx.db.transaction(async (trx) => {
+        const id = input.id ?? crypto.randomUUID();
+        const promises = [];
+
+        // 创建页面
+        promises.push(
+          trx.insert(pages).values({
+            ...input,
+            id,
+            createdById: ctx.session.user.id,
+          }),
+        );
+
+        /* 页面路径 */
+        // 插入自引用路径
+        promises.push(
+          trx.insert(pagesPath).values({
+            ancestor: id,
+            descendant: id,
+            depth: 0,
+          }),
+        );
+
+        // 存在父页面时 添加所有父页面到当前页面的路径
+        const parentId = input.parentId;
+        if (parentId) {
+          // 获取父页面在当前闭包表中作为子节点的所有路径记录 将depth+1后为当前节点插入路径
+          const addPath = async ({
+            parentId,
+            id,
+          }: {
+            parentId: string;
+            id: string;
+          }) => {
+            const parentPaths = await trx.query.pagesPath.findMany({
+              where: (fields, operators) => {
+                return operators.and(
+                  // 查找父页面的所有路径
+                  operators.eq(fields.descendant, parentId),
+                );
+              },
+            });
+            await trx.insert(pagesPath).values(
+              parentPaths.map((path) => ({
+                ancestor: path.ancestor,
+                descendant: id,
+                depth: path.depth + 1,
+              })),
+            );
+          };
+          promises.push(addPath({ parentId, id }));
+        }
+        await Promise.all(promises);
+      });
     }),
 
   update: protectedProcedure
@@ -186,7 +232,6 @@ export const pageRouter = createTRPCRouter({
         id: z.string().describe("页面id"),
         name: z.string().optional().describe("页面名称"),
         content: z.string().optional().describe("页面内容"),
-        isPinned: z.boolean().optional().describe("是否置顶"),
       }),
     )
     .mutation(async ({ ctx, input }) => {
