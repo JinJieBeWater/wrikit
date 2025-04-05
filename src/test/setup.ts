58dc3a3c57f7cf8556e1c768c55cb5449e65a39a
JinJieBeWater <cjj20030813@qq.com>
2025-04-06 04:41:09 +0800

    ✨ feat(test): add benchmark test and improve post router test
    - 【test】新增基准测试，用于评估代码性能
    - 【test】优化 post 路由测试，增加用户创建和获取最新 post 的测试用例
    - 【build】添加 drizzle-seed 依赖，用于生成测试数据
    - 【chore】添加测试基准命令

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -2,6 +2,7 @@ import { beforeAll, vi } from "vitest";
 import type * as schema from "@/server/db/schema";
 import type { PostgresJsDatabase } from "drizzle-orm/postgres-js";
 import { setupDockerTestDb } from "./server/utils/setupDockerTestDb";
+import { seed } from "drizzle-seed";
 
 vi.mock("react", async (importOriginal) => {
 	const testCache = <T extends (...args: Array<unknown>) => unknown>(func: T) =>
@@ -18,6 +19,8 @@ let db: PostgresJsDatabase<typeof schema>;
 beforeAll(async () => {
 	const { db: testDb } = await setupDockerTestDb();
 	db = testDb;
+
+	// await seed(db, schema, { count: 1000 });
 });
 
 export { db };
